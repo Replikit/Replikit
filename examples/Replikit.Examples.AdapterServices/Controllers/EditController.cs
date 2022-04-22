b8db2e9547282b63a1c108fc437daff30ab1361f
Exeteres <exeteres@yandex.ru>
2022-04-22 15:09:06 +0700

    chore: Update examples

--- examples/Replikit.Examples.AdapterServices/Controllers/EditController.cs
+++ examples/Replikit.Examples.AdapterServices/Controllers/EditController.cs
@@ -1,15 +1,12 @@
 using Replikit.Abstractions.Messages.Models;
-using Replikit.Abstractions.Messages.Services;
 using Replikit.Core.Controllers;
 using Replikit.Core.Controllers.Patterns;
-using Replikit.Core.Requirements;
 
 namespace Replikit.Examples.AdapterServices.Controllers;
 
 public class EditController : Controller
 {
     [Command("edit")]
-    [RequireFeatures(MessageServiceFeatures.Edit)]
     public async Task<OutMessage?> Edit(string text)
     {
         if (Message.Reply is null || Message.Reply.AccountId != Adapter.Id.BotId)
